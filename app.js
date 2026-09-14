60878ebaede4c60d45c0a5704ae0f0ef2acfa66c
FadhilDaniswara <nayafd21@gmail.com>
2026-09-14 20:35:19 +0700

    Ini  Bagian 19 Event Handling

--- app.js
+++ app.js
@@ -95,3 +95,22 @@ function render() {
   renderProducts(result);
 }
 render();
+
+//start bagian 19 yohh
+const searchInput = document.querySelector("#search-input");
+searchInput?.addEventListener("input", (e) => {
+  state.search = e.target.value;
+  render();
+});
+
+const categorySelect = document.querySelector("#category-select");
+categorySelect?.addEventListener("change", (e) => {
+  state.category = e.target.value;
+  render();
+});
+
+const sortSelect = document.querySelector("#sort-select");
+sortSelect?.addEventListener("change", (e) => {
+  state.sortBy = e.target.value;
+  render();
+});
